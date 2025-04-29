359a2accfe9270b197cf1c628f3ab43fde1ef7d7
marcoevelle <tzmarcarello@yahoo.com>
2025-04-28 22:30:35 -0400

    c4l2 complete

--- src/blocks.py
+++ src/blocks.py
@@ -1,10 +1,45 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    QUOTE = "quote"
+    CODE = "code"
+    ULIST = "unordered_list"
+    OLIST = "ordered_list"
+
 def markdown_to_blocks(markdown):
-    blocks = []
-    stripped_blocks = []
-    split_blocks = markdown.split("\n\n")
-    for split_block in split_blocks:
-        stripped_blocks.append(split_block.strip('\n'))
-    for block in stripped_blocks:
-        if block != "":
-            blocks.append(block)
-    return blocks
+    blocks = markdown.split("\n\n")
+    filtered_blocks = []
+    for block in blocks:
+        if block == "":
+            continue
+        block = block.strip()
+        filtered_blocks.append(block)
+    return filtered_blocks
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return BlockType.CODE
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.ULIST
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.OLIST
+    return BlockType.PARAGRAPH
